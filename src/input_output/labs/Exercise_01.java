b28671cb8cef7b743fb3b8788ff41385714f0dd2
dondasasmita <dondasasmita@gmail.com>
2018-02-19 12:19:51 +0700

    completed imput output lab exercise 1

--- src/input_output/labs/Exercise_01.java
+++ src/input_output/labs/Exercise_01.java
@@ -9,3 +9,43 @@ package input_output.labs;
  *
  */
 
+import java.io.*;
+
+public class Exercise_01 {
+
+        public static void main(String[] args) {
+
+            FileInputStream in = null;
+            FileOutputStream out = null;
+
+            try {
+                // initialize
+                in = new FileInputStream("/Users/donda/Documents/CodingNomads/online_java_labs/test_files/another_test.txt");
+                out = new FileOutputStream("/Users/donda/Documents/CodingNomads/online_java_labs/test_files/another_test_copy.txt");
+                int c;
+
+                // while there is data to read in the inputStream "in"
+                while ((c = in.read()) != -1) {
+                    //write the data (byte by byte) to the outputStream "out"
+                    out.write(c);
+                }
+            } catch (IOException ioe) {
+                System.out.println("IOException is caught");
+                ioe.printStackTrace();
+            }
+            finally {
+                // close connections to files
+                try {
+                    if (in != null) {
+                        in.close();
+                    }
+                    if (out != null) {
+                        out.close();
+                    }
+
+                    } catch (IOException ioe) {
+                    System.out.println("IOException is caught - No connection to close. ");
+                }
+            }
+        }
+}
