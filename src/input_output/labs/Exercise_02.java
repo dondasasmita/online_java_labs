6b88f78328cae1fcdcd2b7c64751253d748fd415
dondasasmita <dondasasmita@gmail.com>
2018-02-19 17:48:58 +0700

    completed exercise 2 input output

--- src/input_output/labs/Exercise_02.java
+++ src/input_output/labs/Exercise_02.java
@@ -12,3 +12,62 @@ package input_output.labs;
  *
  */
 
+import java.io.*;
+
+public class Exercise_02 {
+
+    public static void main(String[] args) {
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            // initialize
+            in = new FileInputStream("/Users/donda/Documents/CodingNomads/online_java_labs/test_files/file_to_encrypt.txt");
+            out = new FileOutputStream("/Users/donda/Documents/CodingNomads/online_java_labs/test_files/encrypted_file.txt");
+            int c;
+
+            // while there is data to read in the inputStream "in"
+            while ((c = in.read()) != -1) {
+                //write the data (byte by byte) to the outputStream "out"
+                try {
+                    switch (c) {
+                        case 97:
+                            c = 45;
+                            out.write(c);
+                            break;
+                        case 101:
+                            c = 126;
+                            out.write(c);
+                            break;
+                        default:
+                            out.write(c);
+                    }
+                } catch(IOException ioe) {
+                    System.out.println("IOException is caught. Unable to change character");
+                    ioe.printStackTrace();
+                }
+            }
+        }
+        catch(IOException ioe) {
+            System.out.println("IOException is caught");
+            ioe.printStackTrace();
+            }
+
+            finally{
+                // close connections to files
+                try {
+                    if (in != null) {
+                        in.close();
+                    }
+                    if (out != null) {
+                        out.close();
+                    }
+
+                } catch (IOException ioe) {
+                    System.out.println("IOException is caught - No connection to close. ");
+                }
+        }
+    }
+
+}
---
+++ src/input_output/labs/Exercise_02_version_2.java
@@ -0,0 +1,4 @@
+package input_output.labs;
+
+public class Exercise_02_version_2 {
+}
